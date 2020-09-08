2d30cc80727265d9ce7f167a04a9b75a1b8725e6
crdr-cmck <crdr@cmck.com>
2020-09-08 13:26:22 +0100

    defined player

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -1,7 +1,18 @@
 let gameComplete = false;
 // Define the three constants here
-
+    const name="unknown";
+    const score=0;
+    const items=0;
 // Define the player object here
+class Player{
+
+    constructor (name, score, items){
+        this.name = name;
+        this.score = score;
+        this.items = items;
+    }
+}
+let player = Player(name,score, items);
 
 // Define the Product class - write the Constructor function for Product class here
 
